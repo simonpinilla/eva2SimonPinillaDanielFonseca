530d967e4009dca74ba5df570b995feee8e27e43
simonpinilla <simon.pinilla10@gmail.com>
2023-10-23 15:02:29 -0300

    porductosApp terminada

--- certamen/urls.py
+++ certamen/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productosApp.views import *
+from juegos.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('juguetes/', juguetes),
+    path('electronica/', electronica),
+    path('ropa/', ropa),	
 ]
--- productosApp/views.py
+++ productosApp/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'productosApp/index.html')
+
+def juguetes(request):
+
+    data = {
+        "titulo": "JUGUETES",
+        "producto1": "LEGO STAR WARS",
+        "producto2": "MONOPOLY",
+        "producto3": "LANZADOR NERF",
+        'url': '/www.inacap.cl',
+        'imagen1': 'images/juguetes/producto1.png',
+        'imagen2': 'images/juguetes/producto2.png',
+        'imagen3': 'images/juguetes/producto3.png',
+    }
+    return render(request, 'productosApp/catalogo.html', data)
+
+def electronica(request):
+    data = {
+        "titulo": "ELECTRÓNICA",
+        "producto1": "TV LG",
+        "producto2": "APPLE TV",
+        "producto3": "APPLE WATCH",
+        'url': '/www.inacap.cl',
+        'imagen1': 'images/electronica/producto1.png',
+        'imagen2': 'images/electronica/producto2.png',
+        'imagen3': 'images/electronica/producto3.png',
+    }
+    return render(request, 'productosApp/catalogo.html', data)
+
+def ropa(request):
+    data = {
+        "titulo": "ROPA",
+        "producto1": "POLERON BUFFALO",
+        "producto2": "POLERON MAUI",
+        "producto3": "CAMISA LEVIS",
+        'url': '/www.inacap.cl',
+        'imagen1': 'images/ropa/producto1.png',
+        'imagen2': 'images/ropa/producto2.png',
+        'imagen3': 'images/ropa/producto3.png',
+    }
+    return render(request, 'productosApp/catalogo.html', data)
+
+
